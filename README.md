c38ed5e42f39d9abcd5f24a465e6c8783278bc45
David Kossoglyad <david@logoui.co.il>
2021-07-20 21:11:18 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `tour-heroes`
+# `aurelia-tour-of-heroes`
 
 This project is bootstrapped by [aurelia-cli](https://github.com/aurelia/cli).
 
@@ -31,3 +31,22 @@ Run `npm run build`, or the old way `au build --env prod`.
 Run `au test` (or `au jest`).
 
 To run in watch mode, `au test --watch` or `au jest --watch`.
+
+## Integration (e2e) tests
+
+You need the app running for integration test.
+
+First, run `au run` and keep it running.
+
+Then run `au cypress` to run cypress in interactive mode.
+
+To perform a test-run and reports the results, do `au cypress --run`.
+
+To ask the `cypress` to start the application first and then start testing: `au cypress --run --start`
+
+The two following flags are useful when using `--start` flag:
+ * To change dev server port, do `au cypress --start --port 8888`.
+ * To change dev server host, do `au cypress --start --host 127.0.0.1`
+
+
+**PS:** It is also possible to mix the flags `au cypress --run --start --port 7070 --host 127.0.0.1`
--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,30 @@ const cssRules = [
     options: {
       esModule: false
     }
+  },
+  {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        plugins: [
+          'autoprefixer',
+          'cssnano'
+        ]
+      }
+    }
   }
 ];
 
+const sassRules = [
+  {
+    loader: "sass-loader",
+    options: {
+      sassOptions: {
+        includePaths: ['node_modules']
+      }
+    }
+  }
+];
 
 module.exports = ({ production } = {}, {extractCss, analyze, tests, hmr, port, host } = {}) => ({
   resolve: {
@@ -215,6 +236,19 @@ module.exports = ({ production } = {}, {extractCss, analyze, tests, hmr, port, h
         // CSS required in templates cannot be extracted safely
         // because Aurelia would try to require it again in runtime
         use: cssRules
+      },
+      {
+        test: /\.scss$/,
+        use: extractCss ? [{
+          loader: MiniCssExtractPlugin.loader
+        }, ...cssRules, ...sassRules
+        ]: ['style-loader', ...cssRules, ...sassRules],
+        issuer: /\.[tj]s$/i
+      },
+      {
+        test: /\.scss$/,
+        use: [...cssRules, ...sassRules],
+        issuer: /\.html?$/i
       },
       { test: /\.html$/i, loader: 'html-loader' },
       { test: /\.ts$/, loader: "ts-loader" },
