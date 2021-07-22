5887600de69fb24281663f16dca59642309973ee
David Kossoglyad <david@logoui.co.il>
2021-07-22 17:39:14 +0300

    chore: add required dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import {Aurelia} from 'aurelia-framework';
 import * as environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
+import { GlobalValidationConfiguration, validateTrigger } from 'aurelia-validation';
 
 export function configure(aurelia: Aurelia): void {
   aurelia.use
     .standardConfiguration()
+    .plugin(PLATFORM.moduleName('aurelia-validation'), (config: GlobalValidationConfiguration) => {
+      config.defaultValidationTrigger(validateTrigger.changeOrFocusout);
+    })
     .feature(PLATFORM.moduleName('model/index'))
     .feature(PLATFORM.moduleName('data/index'))
     .feature(PLATFORM.moduleName('resources/index'));
