b9ae2b2028707e48d0e0fe6419137e5326e89b53
David Kossoglyad <david@logoui.co.il>
2021-07-22 17:38:33 +0300

    fix(data-provider): data loading is inconsistent

--- src/data/providers/implementation/fake/data-provider.ts
+++ src/data/providers/implementation/fake/data-provider.ts
@@ -4,18 +4,9 @@ import { IDataProvider } from 'data/providers/contracts';
 export class DataProvider implements IDataProvider {
 
   public async getHeroesAsync(): Promise<PersonageDto[]> {
-    return await fetch(`./heroes.json`).then(async (response: Response) => {
-      const text = await response.text();
-      
-      const x = JSON.parse(text);
-      const r = Object.keys(x).map(item => {
-        const d = new PersonageDto();
-        d.name = x[item];
-        return d;
-      });
-
-      return r;
-    });
+    return await fetch(`./heroes.json`)
+                  .then(async (response: Response) => 
+                          (await response.json()).map(item => <PersonageDto>item));
   }
 
   public async getVillainsAsync(): Promise<PersonageDto[]> {
--- src/item.ts
+++ src/item.ts
@@ -1,7 +1,14 @@
 import { EditableObjectViewModel } from "@logofx/aurelia-mvvm-plugin";
+import { autoinject, transient } from "aurelia-dependency-injection";
 import { Hero } from "model";
 
+@autoinject
+@transient(Item)
 export class Item extends EditableObjectViewModel<Hero> {
+
+  constructor(model: Hero) {
+    super(model);
+  }
   
   protected save(model: Hero): Promise<any> {
     throw new Error("Method not implemented.");
