eb4f0e5cfc47e0147a8fe5008c1aa133f928af35
iwwk-alex <A2ty7eauhh>
2021-07-27 11:03:22 +0300

    feature UI

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,5 +1,8 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
+import { FrameworkConfiguration } from 'aurelia-framework';
+import {PLATFORM} from "aurelia-pal";
 
 export function configure(config: FrameworkConfiguration): void {
-  //config.globalResources([]);
+  // config.globalResources(['./page-header/page-header']);
+  // config.aurelia.use
+  //   .feature(PLATFORM.moduleName('./page-header/page-header'))
 }
